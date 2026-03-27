71a738d5db60488e1a5ba35586da3d4665c8acbc
Abdirahim-DMZ <105018657+Abdirahim-DMZ@users.noreply.github.com>
2026-03-27 12:57:53 +0530

    Change s3 bucket for the image url

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
-      { protocol: 'https', hostname: 'visionary-house-uploads.s3.ap-south-1.amazonaws.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'visionary-house-web-app.s3.ap-south-1.amazonaws.com', pathname: '/**' },
     ],
   },
   // Optimize production builds
